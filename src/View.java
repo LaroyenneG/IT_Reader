105ca5c19dc21465ab311da9bd8968ead5a2a8de
guillaume <laroyenneg@gmail.com>
2018-07-12 21:26:21 +0200

    interface

--- src/View.java
+++ src/View.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class View extends JFrame {
 
@@ -17,27 +15,36 @@ public class View extends JFrame {
 
     private GridLayout gridLayout;
 
-    private List<JLabel> jLabelList;
+    private Color backGroundColor;
 
 
     public View() {
 
-        jLabelList = new ArrayList<>();
+        backGroundColor = Color.WHITE;
 
         gridLayout = new GridLayout(0, 1);
 
         statusInfo = new JLabel("Collé dans le prespapier !");
-        jLabelList.add(statusInfo);
+        statusInfo.setHorizontalAlignment(SwingConstants.CENTER);
+        statusInfo.setVerticalAlignment(SwingConstants.CENTER);
 
         screen = new JLabel("152.3 kg");
-        jLabelList.add(screen);
+        screen.setHorizontalAlignment(SwingConstants.CENTER);
+        screen.setVerticalAlignment(SwingConstants.CENTER);
 
-        screen.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
         screen.setFont(new Font("Arial", Font.BOLD, 20));
+        screen.setForeground(Color.CYAN);
+        screen.setBackground(Color.BLACK);
+        screen.setOpaque(true);
 
         messure = new JButton("Mesurer");
+        messure.setHorizontalAlignment(SwingConstants.CENTER);
+        messure.setVerticalAlignment(SwingConstants.CENTER);
 
         auto = new JCheckBox("Mode automatique");
+        auto.setHorizontalAlignment(SwingConstants.CENTER);
+        auto.setVerticalAlignment(SwingConstants.CENTER);
+        auto.setOpaque(false);
 
         panel = new JPanel();
 
@@ -49,29 +56,27 @@ public class View extends JFrame {
         panel.add(auto);
         panel.add(messure);
 
+        panel.setOpaque(true);
+
         menu = new JMenuBar();
 
         setDefaultCloseOperation(3);
 
-        setSize(200, 300);
+        setSize(250, 300);
         setResizable(false);
         setVisible(true);
 
         setJMenuBar(menu);
-
         setContentPane(panel);
 
-        centering();
+        color();
     }
 
 
-    private void centering() {
+    public void color() {
 
-        for (JLabel label : jLabelList) {
-            label.setHorizontalAlignment(SwingConstants.CENTER);
-            label.setVerticalAlignment(SwingConstants.CENTER);
-        }
+        panel.setBorder(BorderFactory.createLineBorder(backGroundColor, 7));
+        screen.setBorder(BorderFactory.createLineBorder(backGroundColor, 5));
+        panel.setBackground(backGroundColor);
     }
-
-
 }
