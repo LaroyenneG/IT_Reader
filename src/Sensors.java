af03690d933203c4260f366ebe0d8c77becfe95a
guillaume <laroyenneg@gmail.com>
2018-07-13 15:16:36 +0200

    sensors

--- src/Sensors.java
+++ src/Sensors.java
@@ -1,3 +1,7 @@
+import gnu.io.CommPortIdentifier;
+
+import java.util.Enumeration;
+
 public class Sensors {
 
     private static Sensors instance = null;
@@ -17,6 +21,21 @@ public class Sensors {
 
     public void connect() {
 
+        Enumeration ports = CommPortIdentifier.getPortIdentifiers();
+        int i = 1;
+        while (ports.hasMoreElements()) {
+            CommPortIdentifier port = (CommPortIdentifier) ports.nextElement();
+            System.out.println("Port n°" + i++);
+            System.out.println("\tNom\t:\t" + port.getName());
+            String type = null;
+            if (port.getPortType() == CommPortIdentifier.PORT_SERIAL) type = "Serie";
+            else type = "Parallèle";
+            System.out.println("\tType\t:\t" + type);
+            String etat = null;
+            if (port.isCurrentlyOwned()) etat = "Possédé par " + port.getCurrentOwner();
+            else etat = "Libre";
+            System.out.println("\tEtat\t:\t" + etat + "\n");
+        }
     }
 
 
